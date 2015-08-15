7909f185ffe0e5fdfb3fb913209aeb8ea114227c
Oleksii Fomenko <oleksii.fomenko@ericsson.com>
2015-08-15 11:47:32 +0300

    Location polling works

--- src/org/byako/group_tracker/MainActivity.java
+++ src/org/byako/group_tracker/MainActivity.java
@@ -7,18 +7,27 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity implements
-		GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+		GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
 	private GoogleApiClient mGoogleApiClient;
 	private Location mLastLocation;
+	private boolean isStarted;
+	private String attendeeName;
+	private String eventName;
+	private Location mCurrentLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +62,20 @@ public class MainActivity extends Activity implements
 
 	@Override
 	public void onConnected(Bundle connectionHint) {
-		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-		if (mLastLocation != null) {
-			setStatusText(String.valueOf(mLastLocation.getLatitude()) + ":" + String.valueOf(mLastLocation.getLongitude()));
+//		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+		LocationRequest mLocationRequest = new LocationRequest();
+		mLocationRequest.setInterval(10000);
+		mLocationRequest.setFastestInterval(5000);
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+		LocationServices.FusedLocationApi.requestLocationUpdates(
+				mGoogleApiClient, mLocationRequest, this);
+		setLocationText("Updating location...");
+/*		if (mLastLocation != null) {
+			setLocationText(String.valueOf(mLastLocation.getLatitude()) + ":" + String.valueOf(mLastLocation.getLongitude()));
 		} else {
 			setStatusText("ERROR getting last location");
-		}
+		}*/
 	}
 
 	@Override
@@ -73,14 +90,55 @@ public class MainActivity extends Activity implements
 	private void setStatusText(String newLabel) {
 		((TextView)findViewById(R.id.statusViewId)).setText(newLabel);
 	}
-
-	public void onStartButtonClicked(View v) {
-		//((TextView)findViewById(R.id.statusViewId)).setText("Started");
-		mGoogleApiClient.connect();
+	private void setLocationText(String newLabel) {
+		((TextView)findViewById(R.id.locationViewId)).setText(newLabel);
 	}
 
-	public void onStopButtonClicked(View v) {
-		//((TextView)findViewById(R.id.statusViewId)).setText("Stopped");
-		mGoogleApiClient.disconnect();
+	public void onStartStopButtonClicked(View v) {
+		//((TextView)findViewById(R.id.statusViewId)).setText("Started");
+		if (!isStarted) {
+			mGoogleApiClient.connect();
+			attendeeName = findViewById(R.id.attendeeName).toString();
+			findViewById(R.id.attendeeName).setEnabled(false);
+			eventName = findViewById(R.id.eventName).toString();
+			findViewById(R.id.eventName).setEnabled(false);
+
+			isStarted = true;
+			setStatusText("Started");
+		} else {
+			if (mGoogleApiClient.isConnected()) {
+				LocationServices.FusedLocationApi.removeLocationUpdates(
+						mGoogleApiClient, this);
+				mGoogleApiClient.disconnect();
+			}
+			findViewById(R.id.attendeeName).setEnabled(true);
+			findViewById(R.id.eventName).setEnabled(true);
+			isStarted = false;
+			setStatusText("Stopped");
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		mCurrentLocation = location;
+//		mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+		setLocationText(location.getLatitude() + " : " + location.getLongitude());
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (mGoogleApiClient.isConnected()) {
+			LocationServices.FusedLocationApi.removeLocationUpdates(
+					mGoogleApiClient, this);
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+//		if (mGoogleApiClient.isConnected() && isStarted) {
+//			startLocationUpdates();
+//		}
 	}
 }
